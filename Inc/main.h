63dcce0e3ad1b375cf49af266ce7ea4080e29a63
Kshitij <kshitij.vaze@tor.ai>
2025-12-04 09:43:28 +0530

    Commit from the Work Setup

--- Inc/main.h
+++ Inc/main.h
@@ -8,5 +8,33 @@
 #ifndef MAIN_H_
 #define MAIN_H_
 
+#define TASK_STACK_SIZE				1024U
+#define SCHEDULER_STACK_SIZE		1024U
 
+#define SRAM_START					0x20000000U
+#define SRAM_SIZE					(128*1024)
+#define SRAM_END					((SRAM_START)+(SRAM_SIZE))
+
+#define TASK_1_STACK_START			(SRAM_END)
+#define TASK_2_STACK_START			((SRAM_END)-(1*TASK_STACK_SIZE))
+#define TASK_3_STACK_START			((SRAM_END)-(2*TASK_STACK_SIZE))
+#define TASK_4_STACK_START			((SRAM_END)-(3*TASK_STACK_SIZE))
+#define SCHEDULER_STACK_START		((SRAM_END)-(4*TASK_STACK_SIZE))
+
+#define HSI_CLOCK					16000000U
+#define SYSTICK_TIMER_CLOCK			HSI_CLOCK
+uint32_t timer_freq = 1000U;
+#define MAX_TASKS 4
+uint32_t psp_of_tasks[MAX_TASKS] = { TASK_1_STACK_START, TASK_2_STACK_START,
+		TASK_3_STACK_START, TASK_4_STACK_START };
+
+#define dummy_xpsr					0x01000000U
+
+uint32_t task_handlers[MAX_TASKS];
+
+void BusFault_Handler(void);
+void MemManage_Handler(void);
+void HardFault_Handler(void);
+
+uint32_t current_task = 0;
 #endif /* MAIN_H_ */
--- Src/main.c
+++ Src/main.c
@@ -23,35 +23,7 @@
 #if !defined(__SOFT_FP__) && defined(__ARM_FP)
 #warning "FPU is not initialized, but the project is compiling for an FPU. Please initialize the FPU before use."
 #endif
-#define TASK_STACK_SIZE				1024U
-#define SCHEDULER_STACK_SIZE		1024U
 
-#define SRAM_START					0x20000000U
-#define SRAM_SIZE					(128*1024)
-#define SRAM_END					((SRAM_START)+(SRAM_SIZE))
-
-#define TASK_1_STACK_START			(SRAM_END)
-#define TASK_2_STACK_START			((SRAM_END)-(1*TASK_STACK_SIZE))
-#define TASK_3_STACK_START			((SRAM_END)-(2*TASK_STACK_SIZE))
-#define TASK_4_STACK_START			((SRAM_END)-(3*TASK_STACK_SIZE))
-#define SCHEDULER_STACK_START		((SRAM_END)-(4*TASK_STACK_SIZE))
-
-#define HSI_CLOCK					16000000U
-#define SYSTICK_TIMER_CLOCK			HSI_CLOCK
-uint32_t timer_freq = 1000U;
-#define MAX_TASKS 4
-uint32_t psp_of_tasks[MAX_TASKS] = { TASK_1_STACK_START, TASK_2_STACK_START,
-		TASK_3_STACK_START, TASK_4_STACK_START };
-
-#define dummy_xpsr					0x01000000U
-
-uint32_t task_handlers[MAX_TASKS];
-
-void BusFault_Handler(void);
-void MemManage_Handler(void);
-void HardFault_Handler(void);
-
-uint32_t current_task = 0;
 
 void task1_handler(void) {
 	while (1) {
